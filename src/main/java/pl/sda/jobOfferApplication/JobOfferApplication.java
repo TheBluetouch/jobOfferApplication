805c9f5e9e70e2b70d679fbf57bde1494474c07e
Bluetouch <bogusz28@wp.pl>
2020-08-01 11:59:35 +0200

    Initial commit

---
+++ src/main/java/pl/sda/jobOfferApplication/JobOfferApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.jobOfferApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOfferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/jobOfferApplication/JobOfferApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.jobOfferApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
